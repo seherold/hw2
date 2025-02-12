4f319422d8e2bbdab5f5459bc7617d17ccd47821
Sumner Herold <seherold@gremlin01.beocat.ksu.edu>
2025-02-11 19:21:05 -0600

    Added tests, able to run them with executable

--- test/tests.cpp
+++ test/tests.cpp
@@ -19,14 +19,66 @@ extern "C"
 **/
 /*TEST (first_come_first_serve, BasicPCBs) {
 	
-	ProcessControlBlock_t newPCB1;
-	ProcessControlBlock_t newPCB2;
-	ProcessControlBlock_t newPCB3;
+	ProcessControlBlock_t newPCB1 = {.remaining_burst_time = 5, .priority = 0, .arrival = 0, .started = false};
+	ProcessControlBlock_t newPCB2 = {.remaining_burst_time = 3, .priority = 0, .arrival = 1, .started = false};
+	ProcessControlBlock_t newPCB3 = {.remaining_burst_time = 8, .priority = 0, .arrival = 2, .started = false};
+	
+
+	
 
 	ScheduleResult_t result;
 
 	EXPECT_EQ(true,first_come_first_serve(ready_queue,result));
 }*/
+
+TEST (first_come_first_serve, SameArrivalTime) 
+{
+	ProcessControlBlock_t newPCB1 = {.remaining_burst_time = 5, .priority = 0, .arrival = 0, .started = false};
+	ProcessControlBlock_t newPCB2 = {.remaining_burst_time = 3, .priority = 0, .arrival = 0, .started = false};
+	ProcessControlBlock_t newPCB3 = {.remaining_burst_time = 8, .priority = 0, .arrival = 0, .started = false};
+	
+	dyn_array_t* ready_queue = dyn_array_create(3, sizeof(ProcessControlBlock_t), NULL);
+	
+	dyn_array_push_back(ready_queue, &newPCB1);
+	dyn_array_push_back(ready_queue, &newPCB2);
+	dyn_array_push_back(ready_queue, &newPCB3);
+	
+	ScheduleResult_t result = {.average_waiting_time = 0, .average_turnaround_time = 0, .total_run_time = 0};
+	
+	EXPECT_EQ(true,first_come_first_serve(ready_queue, &result));
+	
+	EXPECT_EQ(result.total_run_time, 16UL);
+	EXPECT_NEAR(result.average_waiting_time, 4.33, 0.01);
+	EXPECT_NEAR(result.average_turnaround_time, 9.67, 0.01);
+	
+	dyn_array_destroy(ready_queue);
+}
+
+// if ready_queue == NULL
+TEST (first_come_first_serve, ReadyQueueNULL) 
+{
+	ScheduleResult_t result = {.average_waiting_time = 0, .average_turnaround_time = 0, .total_run_time = 0};
+	EXPECT_EQ(false,first_come_first_serve(NULL, &result));
+}
+
+
+// if result == NULL
+TEST (first_come_first_serve, ResultNULL) 
+{
+	ProcessControlBlock_t newPCB1 = {.remaining_burst_time = 5, .priority = 0, .arrival = 0, .started = false};
+	ProcessControlBlock_t newPCB2 = {.remaining_burst_time = 3, .priority = 0, .arrival = 0, .started = false};
+	ProcessControlBlock_t newPCB3 = {.remaining_burst_time = 8, .priority = 0, .arrival = 0, .started = false};
+	
+	dyn_array_t* ready_queue = dyn_array_create(3, sizeof(ProcessControlBlock_t), NULL);
+	
+	dyn_array_push_back(ready_queue, &newPCB1);
+	dyn_array_push_back(ready_queue, &newPCB2);
+	dyn_array_push_back(ready_queue, &newPCB3);
+	
+	EXPECT_EQ(false,first_come_first_serve(ready_queue, NULL));
+	
+	dyn_array_destroy(ready_queue);
+}
 
 
 unsigned int score;
