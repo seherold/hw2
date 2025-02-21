61f1c515c270ce2f54b338e322b029faac22de8a
Sumner Herold <seherold@gremlin01.beocat.ksu.edu>
2025-02-21 16:28:18 -0600

    Debugging analysis.c

--- src/analysis.c
+++ src/analysis.c
@@ -19,8 +19,8 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	abort();		// REPLACE ME with implementation.
-	/*
+	//abort();		// REPLACE ME with implementation.
+	
 	// Load process control blocks from binary file passed at the command line into a dyn_array (this is your ready queue).
 	char* file = argv[1];
 	dyn_array_t* ready_queue = load_process_control_blocks(file);
@@ -29,37 +29,37 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	ScheduleResult_t* result;
+	ScheduleResult_t result = {.average_waiting_time = 0, .average_turnaround_time = 0, .total_run_time = 0};
 
 	// Execute your scheduling algorithm to collect the statistics
 	char* algorithm = argv[2];
 	bool schedulingSuccess;
 
-	if (strncmp(algorithm, 'FCFS', 4) == 0); // use defines
+	if (strncmp(algorithm, FCFS, 4) == 0) // use defines
 	{
-		schedulingSuccess = first_come_first_serve(ready_queue, result);
+		schedulingSuccess = first_come_first_serve(ready_queue, &result);
 	}
-	else if (strncmp(algorithm, 'SJF', 3) == 0);
+	else if (strncmp(algorithm, SJF, 3) == 0)
 	{
-		schedulingSuccess = shortest_job_first(ready_queue, result);
+		schedulingSuccess = shortest_job_first(ready_queue, &result);
 	}
-	else if (strncmp(algorithm, 'PRI', 3) == 0);
+	else if (strncmp(algorithm, P, 3) == 0)
 	{
-		schedulingSuccess = priority(ready_queue, result);
+		schedulingSuccess = priority(ready_queue, &result);
 	}
-	else if (strncmp(algorithm, 'RR', 2) == 0);
+	else if (strncmp(algorithm, RR, 2) == 0)
 	{
 		if (argc < 4)
 		{
 			dyn_array_destroy(ready_queue);
 			return EXIT_FAILURE;
 		}
-		size_t quantum = argv[3];
-		schedulingSuccess = round_robin(ready_queue, result, quantum);
+		size_t quantum = (size_t)argv[3];
+		schedulingSuccess = round_robin(ready_queue, &result, quantum);
 	}
-	else if (strncmp(algorithm, 'SRTF', 4) == 0);
+	else if (strncmp(algorithm, "SRTF", 4) == 0)
 	{
-		schedulingSuccess = shortest_remaining_time_first(ready_queue, result);
+		schedulingSuccess = shortest_remaining_time_first(ready_queue, &result);
 	}
 	else // algorithm string doesn't match any known algorithm
 	{
@@ -80,9 +80,9 @@ int main(int argc, char **argv)
 
 
 	// Report your times to STDOUT using either printf or fprintf (copy these to the README.md file)
-	if (printf("Average Waiting Time: %f\n", result->average_waiting_time) ||
-	printf("Average Turnaround Time: %f\n", result->average_turnaround_time) ||
-	printf("Total Run Time: %lu\n", result->total_run_time))
+	if (printf("Average Waiting Time: %f\n", result.average_waiting_time) < 0 ||
+	printf("Average Turnaround Time: %f\n", result.average_turnaround_time) < 0 ||
+	printf("Total Run Time: %lu\n", result.total_run_time) < 0) // error writing to STDOUT
 	{
 		return EXIT_FAILURE;
 	}
@@ -93,15 +93,14 @@ int main(int argc, char **argv)
         return EXIT_FAILURE; 
     } 
    
-	if (fprintf(fptr, "Average Waiting Time: %f\n", result->average_waiting_time) ||
-	fprintf(fptr, "Average Turnaround Time: %f\n", result->average_turnaround_time) ||
-	fprintf(fptr, "Total Run Time: %lu\n", result->total_run_time))
+	if (fprintf(fptr, "Average Waiting Time: %f\n", result.average_waiting_time) < 0 ||
+	fprintf(fptr, "Average Turnaround Time: %f\n", result.average_turnaround_time) < 0 ||
+	fprintf(fptr, "Total Run Time: %lu\n", result.total_run_time) < 0) // error writing to README.md
 	{
 		return EXIT_FAILURE;
 	}
 
     fclose(fptr);
-	*/
 
 	return EXIT_SUCCESS;
 }
